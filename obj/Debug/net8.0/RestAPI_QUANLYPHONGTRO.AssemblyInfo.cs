7b01d67d2722a6537e477c474f05adecbf96cbe5
Ann <annrok@gmail.com>
2025-11-30 02:05:26 +0700

    test

--- obj/Debug/net8.0/RestAPI_QUANLYPHONGTRO.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestAPI_QUANLYPHONGTRO.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestAPI_QUANLYPHONGTRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54dfbad9043f8315981430036cb6626f6464dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestAPI_QUANLYPHONGTRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestAPI_QUANLYPHONGTRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
