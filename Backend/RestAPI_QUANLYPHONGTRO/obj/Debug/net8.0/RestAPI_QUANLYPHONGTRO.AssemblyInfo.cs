0ca16511a8d6ad70bf0d7f12711297ed379a5104
Quang <qpham159753@gmail.com>
2025-12-22 11:23:33 +0700

    Lưu thay đổi

--- Backend/RestAPI_QUANLYPHONGTRO/obj/Debug/net8.0/RestAPI_QUANLYPHONGTRO.AssemblyInfo.cs
+++ Backend/RestAPI_QUANLYPHONGTRO/obj/Debug/net8.0/RestAPI_QUANLYPHONGTRO.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestAPI_QUANLYPHONGTRO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b4dec84b1c2c63827b53d91689f5edb3f254bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75b9a11e98bbc3e97d38bc0fc3accd53be747fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestAPI_QUANLYPHONGTRO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestAPI_QUANLYPHONGTRO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
